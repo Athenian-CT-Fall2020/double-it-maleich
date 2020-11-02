7da6f184bcafb21aecfada94cc3aa7d6eac64e01
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-11-02 07:06:30 -0800

    Initial commit

---
+++ my_code.py
@@ -0,0 +1,14 @@
+# Collaborators: 
+
+def double_it(x):
+    product =    # add code here
+    
+    return product
+
+
+if __name__ == '__main__':
+    print(double_it(6))
+
+    # After you are satisfied with your results, use input() to prompt the user for a value:
+    #v = int(input("Value to double: "))
+    #print(double_it(v))
