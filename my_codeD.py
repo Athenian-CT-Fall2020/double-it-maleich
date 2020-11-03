239e22b26644dbba3a707ef38ed603d36628baec
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-11-03 11:11:19 -0800

    finished

--- my_codeD.py
+++ my_codeD.py
@@ -1,14 +1,13 @@
-# Collaborators:
+# Collaborators: D period!
 
 def double_it(x):
-    product =  # add code here
-
+    product = x * 2
     return product
 
 
 if __name__ == '__main__':
-    print(double_it(6))
+    # print(double_it(300))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
-    # v = int(input("Value to double: "))
-    # print(double_it(v))
+    v = int(input("Value to double: "))
+    print(double_it(v))
